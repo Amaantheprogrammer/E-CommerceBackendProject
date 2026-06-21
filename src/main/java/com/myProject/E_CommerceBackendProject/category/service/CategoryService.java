cd0c3536229260b8ab6dc72aeda3750af4183840
Amaantheprogrammer <your-email@example.com>
2026-06-21 21:42:48 +0530

    category creation

--- src/main/java/com/myProject/E_CommerceBackendProject/category/service/CategoryService.java
+++ src/main/java/com/myProject/E_CommerceBackendProject/category/service/CategoryService.java
@@ -3,10 +3,14 @@ package com.myProject.E_CommerceBackendProject.category.service;
 import java.util.List;
 
 import com.myProject.E_CommerceBackendProject.category.dto.CategoryDto;
+import com.myProject.E_CommerceBackendProject.category.dto.NewCategoryDto;
 
 public interface CategoryService {
+
     List<CategoryDto> getAllCategories();
 
     CategoryDto getCategoryById(Long id);
+
+    CategoryDto createCategory(NewCategoryDto newCategoryDto);
 
 }
--- src/main/java/com/myProject/E_CommerceBackendProject/category/service/CategoryServiceImpl.java
+++ src/main/java/com/myProject/E_CommerceBackendProject/category/service/CategoryServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.myProject.E_CommerceBackendProject.category.dto.CategoryDto;
+import com.myProject.E_CommerceBackendProject.category.dto.NewCategoryDto;
 import com.myProject.E_CommerceBackendProject.category.entity.Category;
 import com.myProject.E_CommerceBackendProject.category.repository.CategoryRepository;
 import com.myProject.E_CommerceBackendProject.exception.ResourceNotFoundException;
@@ -29,6 +30,12 @@ public class CategoryServiceImpl implements CategoryService {
         Category category = categoryRepository.findById(id)
                     .orElseThrow(() -> new ResourceNotFoundException("Category not found with ID: " + id));
         return mapToDto(category);
+    }
+    
+    @Override
+    public CategoryDto createCategory(NewCategoryDto newCategoryDto) {
+        Category category = Category.builder().name(newCategoryDto.getName()).build();
+        return mapToDto(categoryRepository.save(category));
     }
 
     private CategoryDto mapToDto(Category category) {
