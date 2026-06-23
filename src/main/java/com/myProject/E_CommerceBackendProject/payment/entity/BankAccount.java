3996c13fb1dc8409e2aa8888db523aa770c58075
Amaantheprogrammer <your-email@example.com>
2026-06-23 23:03:30 +0530

    Add BankAccount entity with balance and user association

---
+++ src/main/java/com/myProject/E_CommerceBackendProject/payment/entity/BankAccount.java
@@ -0,0 +1,39 @@
+package com.myProject.E_CommerceBackendProject.payment.entity;
+
+import java.math.BigDecimal;
+
+import com.myProject.E_CommerceBackendProject.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "bank_accounts")
+@Entity
+@Builder
+public class BankAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal balance;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @ToString.Exclude
+    private User user;
+}
