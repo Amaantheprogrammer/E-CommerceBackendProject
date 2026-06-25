ff38818be7c368ccd3281c7f3773ed95e3491f71
Amaantheprogrammer <your-email@example.com>
2026-06-26 05:23:34 +0530

    Implement JWT token generation and validation methods; add AuthResponse DTO for authentication response

--- src/main/java/com/myProject/E_CommerceBackendProject/security/JwtService.java
+++ src/main/java/com/myProject/E_CommerceBackendProject/security/JwtService.java
@@ -3,10 +3,13 @@ package com.myProject.E_CommerceBackendProject.security;
 
 import java.security.Key;
 import java.util.Date;
-import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -17,19 +20,49 @@ public class JwtService {
     private static final String SECRET_KEY = "3cfa76ef5246212458a2307ef6901a5f6e804f3263725b83cfcd091ea285fef1";
     private static final long JWT_EXPIRATION = 1000 * 60 * 60; // 1 hour
 
-    public String generateToken(String username) {
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return Jwts.builder()
-            .setSubject(username)
-            .setIssuedAt(new Date())
-            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 3600))
-            .addClaims(new HashMap<>())
+            .setClaims(extraClaims)
+            .setSubject(userDetails.getUsername())
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
             .signWith(getSignedKey(), SignatureAlgorithm.HS256)
             .compact();
     }
     
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+            .setSigningKey(getSignedKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
     private Key getSignedKey() {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
     }
 }
 
-// Continue from here 
+// Continue from here: Make isTokenValid method
